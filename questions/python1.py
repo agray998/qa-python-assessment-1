9f671346d355bfa4ba76057e76dcaa867373d706
htr-volker <htr.volker@gmail.com>
2021-04-27 18:46:24 +0100

    init commit

---
+++ questions/python1.py
@@ -0,0 +1,257 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# INSTRUCTIONS
+
+	# In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
+	#
+	# 	<QUESTION>
+	#
+	# 	<EXAMPLES>
+	#
+	# 	<HINT>
+
+	# You are allowed access to the internet for this assessment, but remember you could use the DOCUMENTATION that comes bundled with your Python installation. 
+	# Every command has help documentation. To read it:
+	# 	1. Access Python from your CLI
+	# 	2. Type help(<command>) and hit enter, where <command> is the command you want help with. For example: help(str)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 1>
+
+	# Define a function that can accept two strings as input and returns the string with maximum length to the console. 
+	
+	# If two strings have the same length, then the function should return both strings separated by a " ".
+
+	# In this case, the strings should be returned in the same order in which they were given.
+
+	# <EXAMPLES>
+
+	# one("hi","hello") → "hello"
+	# one("three", "two") → "three"
+	# one("three", "hello") → "three hello"
+
+	# <HINT>
+
+	# What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
+
+def one(input1, input2):
+	return ""
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 2>
+
+    # given a number
+	# if this number is divisible by 3 return "fizz"
+	# if this number is divisible by 5 return "buzz"
+	# if this number is divisible by both 3 and 5 return "fizzbuzz"
+	# if this number is not divisible by 3 or 5 return "null"
+	    
+    # <EXAMPLES>
+
+	# two(3) → "fizz"
+	# two(10) → "buzz"
+	# two(15) → "fizzbuzz"
+	# two(8) → "null"
+
+	# <HINT>
+
+	# No hints for this question
+
+def two(arg1):
+	return "null"
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 3>
+
+    # Write a function which returns the integer number of vowels in a given string. 
+    # You should ignore case.
+
+	# <EXAMPLES>
+
+    # three("Hello") → 2
+    # three("hEelLoooO") → 6
+
+	# <HINTS>
+
+	# How do we ignore case in a String? help(str) may offer some insight.
+
+def three(input):
+    return 0
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 4>
+
+    # There is a well known mnemonic which goes "I before E, except after C", which is used to determine which order "ei" or "ie" should be in a word.
+    
+    # Write a function which returns the boolean True if a string follows the mnemonic, and returns the boolean False if not.
+
+	# <EXAMPLES>
+
+    # four("ceiling") → True
+    # four("believe") → True
+    # four("glacier") → False
+    # four("height") → False
+
+	# <HINT>
+
+	# Step through the logic here in order to solve the problem, you may find help(range) helpful.
+
+
+def four(input):
+    return False
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 5>
+
+	# Write a function which takes an input (between 1 and 10 inclusive) and multiplies it by all the numbers before it.
+	# eg If the input is 4, the function calculates 4x3x2x1 = 24 
+
+	# <EXAMPLES>
+
+	# five(1) → 1
+	# five(4) → 24
+	# five(8) → 40320
+
+	# <HINT>
+
+	# You may need to create a list of numbers from 0 to i, take a look at help(range).
+
+def five(input):
+	return 1
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 6>
+
+    # Given a string, int and a char, return a boolean value if the 'nth' 
+    # (represented by the int provided) char of the String supplied is the same as the char supplied.
+    # The int provided will NOT always be less than than the length of the String.
+    # IGNORE case and Whitespace. 
+    
+    # <EXAMPLES>
+
+	# six("The",2,'h') → True
+	# six("AAbb",1,'b') → False
+	# six("Hi-There",10,'e') → False
+
+	# <HINT>
+
+	# How do we find the length of a container, take a look at help(len), you will also need to look at help(str) for String manipulation.
+ 
+def six(string, int, char):
+	return False
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 7>
+
+    # Given a string and a char, returns the position in the String where the char first appears.
+    # Ensure the positions are numbered correctly, please refer to the examples for guidance.
+    # DO NOT ignore case
+    # IGNORE whitespace
+    # If the char does not occur, return the number -1.
+    
+    # <EXAMPLES>
+
+	# seven("This is a Sentence","s") → 4
+	# seven("This is a Sentence","S") → 8
+	# seven("Fridge for sale","z") → -1
+
+	# <HINT>
+
+	# Take a look at the documentation for Strings, List and range.
+
+def seven(inputString, char):
+	return -1
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 8>
+
+    # Given a string seperate the string into the individual numbers present, then add each digit of each number to get a final value for each number
+
+	# String example = "55 72 86"
+	
+	# "55" will = the integer 10
+	# "72" will = the integer 9
+	# "86" will = the integer 14
+	
+	# You then need to return the highest value, in the example above this would be 14.
+	 
+    # <EXAMPLES>
+
+	# eight("55 72 86") → 14
+	# eight("15 72 80 164") → 11
+	# eight("555 72 86 45 10") → 15
+
+	# <HINT>
+
+	# help(int) for working with numbers and help(str) for working with Strings.
+
+def eight(arg1):
+	return 0
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 9>
+
+    # Return the string that is between the first and last appearance of "bert" in the given string
+	
+	# Return the empty string "" if there is not 2 occurances of "bert" 
+	
+	# IGNORE CASE
+    
+    # <EXAMPLES>
+
+	# nine("bertclivebert") → "clive"
+	# nine("xxbertfridgebertyy") → "fridge"
+	# nine("xxBertfridgebERtyy") → "fridge"
+	# nine("xxbertyy") → ""
+	# nine("xxbeRTyy") → ""
+
+	# <HINT>
+
+	# What was the name of the function we have seen to seperate a String? How can we make a string all upper or lower case?
+	
+	# Use your CLI to access the Python documentation and get help manipulating strings - help(str).
+
+def nine(input):
+	return ""
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+	# <QUESTION 10>
+
+    # Given a large string that represents a csv, the structure of each record will be as follows:
+    
+    # owner,nameOfFile,encrypted?,fileSize
+    
+    # "Bert,helloWorld.py,True,1447,Bert,strings.py,False,1318,Jeff,dice.py,False,1445"
+    
+    # For each record, if it is not encrypted, return the names of the owners of the files in a String Array.
+    # Do not include duplicate names.
+	# If all records are encrypted, return an empty Array.
+    
+    # <EXAMPLES>
+    
+    # ten("Jeff,random.py,False,1445") → ["Jeff"]
+	# ten("Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445") → ["Jeff"]
+	# ten("Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445") → ["Bert","Jeff"]
+    # ten("Bert,prime.py,True,1447,Bert,ISBN.py,False,1318,Jeff,OOP.py,False,1445") → ["Bert","Jeff"]
+    
+	# <HINT>
+
+	# Dont't forget, False is a String, not a Boolean value in the Tests above.
+
+	# help(str) and help(list), you might also need to use a function that can create a list of numbers for you, try help(range).
+
+def ten(input):
+	return []
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
---
+++ tests/assessment_test.py
@@ -0,0 +1,72 @@
+import pytest
+from questions import python1_answers as python1
+
+def test_one():
+    assert python1.one("hi","hello") == "hello"
+    assert python1.one("three", "two") == "three"
+    assert python1.one("three", "hello") == "three hello"
+    assert python1.one("echo", "print") == "print"
+    assert python1.one("fire","rib") == "fire"
+
+def test_two():
+    assert python1.two(3) == "fizz"
+    assert python1.two(10) == "buzz"
+    assert python1.two(15) == "fizzbuzz"
+    assert python1.two(8) == "null"
+    assert python1.two(75) == "fizzbuzz"
+
+def test_three():
+    assert python1.three("Hello") == 2 
+    assert python1.three("hEelLoooO") == 6
+    assert python1.three("WhitEboarD") == 4
+    assert python1.three("as") == 1
+    assert python1.three("pass") == 1
+
+def test_four():
+    assert python1.four("ceiling") == True
+    assert python1.four("believe") == True
+    assert python1.four("glacier") == False
+    assert python1.four("height") == False
+    assert python1.four("receive") == True
+
+def test_five():
+    assert python1.five(1) == 1
+    assert python1.five(3) == 6
+    assert python1.five(4) == 24
+    assert python1.five(6) == 720
+    assert python1.five(8) == 40320
+
+def test_six():
+    assert python1.six("The",2,"h") == True
+    assert python1.six("AAbb",1,"b") == False
+    assert python1.six("Hi-There",10,"e") == False
+    assert python1.six("HEY",2,"e") == True
+    assert python1.six("on-premise",3,"-") == True
+
+def test_seven():
+    assert python1.seven("This is a Sentence","s") == 4
+    assert python1.seven("This is a Sentence","S") == 8
+    assert python1.seven("Fridge for sale","z") == -1
+    assert python1.seven("I love Python", "L") == -1
+    assert python1.seven("I LOVE PYTHON", "L") == 2
+
+def test_eight():
+    assert python1.eight("55 72 86") == 14
+    assert python1.eight("15 72 80 164") == 11
+    assert python1.eight("555 72 86 45 10") == 15
+    assert python1.eight("98 63 34 1 13") == 17
+    assert python1.eight("98 107 415") == 17
+
+def test_nine():
+    assert python1.nine("bertclivebert") == "clive"
+    assert python1.nine("xxbertfridgebertyy") == "fridge"
+    assert python1.nine("xxBertfridgebERtyy") == "fridge"
+    assert python1.nine("xxbertyy") == ""
+    assert python1.nine("xxbeRTyy") == ""
+
+def test_ten():
+    assert python1.ten("Jeff,random.py,False,1445") == ["Jeff"]
+    assert python1.ten("Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445") == ["Jeff"]
+    assert python1.ten("Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445") == ["Bert","Jeff"]
+    assert python1.ten("Bert,prime.py,True,1447,Bert,ISBN.py,False,1318,Jeff,OOP.py,False,1445") == ["Bert","Jeff"]
+    assert python1.ten("Bert,files.py,True,1447,Bert,tests.py,True,1318,Jeff,app.py,True,1445") == []
