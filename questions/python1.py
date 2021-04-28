b54173e7688d5e0c9912d772850bb631e9f8288f
Adam Gray <a.gray.4797@gmail.com>
2021-04-28 11:10:12 +0100

    Question 2 complete

--- questions/python1.py
+++ questions/python1.py
@@ -65,7 +65,14 @@ def one(input1, input2):
 	# No hints for this question
 
 def two(arg1):
-	return "null"
+    if arg1 % 3 == 0 and arg1 % 5 == 0:
+        return "fizzbuzz"
+    elif arg1 % 3 == 0:
+        return "fizz"
+    elif arg1 % 5 == 0:
+        return "buzz"
+    else:
+	    return "null"
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
